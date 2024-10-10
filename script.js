8fd81648af4d1538a0dcc136737ab1407abd3db7
Cholpon <127111343+cholpoun@users.noreply.github.com>
2024-10-10 22:59:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ hamburgerMenu.addEventListener('click', () => {
     navLinks.classList.toggle('active');
     const expanded = hamburgerMenu.getAttribute('aria-expanded') === 'true';
     hamburgerMenu.setAttribute('aria-expanded', !expanded);
+    document.body.classList.toggle('menu-open');
 });
 
 navLinks.addEventListener('click', (event) => {
     if (event.target.tagName === 'A') {
         navLinks.classList.remove('active');
         hamburgerMenu.setAttribute('aria-expanded', 'false');
+        document.body.classList.remove('menu-open');
     }
 });
